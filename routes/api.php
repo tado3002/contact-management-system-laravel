cfc4da6abaf10bfc8ddc9487eb628a05ebcf0972
Muhammad Murtadlo <muh.murtadlo23@gmail.com>
2025-05-08 16:53:05 +0700

    add users route

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\ApiAuthMiddleware;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*Route::get('/user', function (Request $request) {*/
+/*    return $request->user();*/
+/*})->middleware('auth:sanctum');*/
+
+Route::post('/users', [UserController::class, 'register']);
+Route::post('/users/login', [UserController::class, 'login']);
+
+Route::middleware([ApiAuthMiddleware::class])->group(function () {
+    Route::get('/users/current', [UserController::class, 'get']);
+    Route::patch('/users/current', [UserController::class, 'update']);
+    Route::delete('/users/logout', [UserController::class, 'logout']);
+});
