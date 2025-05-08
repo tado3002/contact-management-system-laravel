6f9d144a6e6e90fcca205f9e7ad832d1f407787d
Muhammad Murtadlo <muh.murtadlo23@gmail.com>
2025-05-08 16:51:24 +0700

    add seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = [
+            'username' => 'tado3002',
+            'password' => Hash::make('mypassword'),
+            'name' => 'tado'
+
+        ];
+        User::create($data);
+    }
+}
