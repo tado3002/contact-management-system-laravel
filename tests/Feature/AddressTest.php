5bc8d62d890f7a3437d8356cc9a73a8d4aa7f3de
Muhammad Murtadlo <muh.murtadlo23@gmail.com>
2025-05-11 00:21:06 +0700

    add Address test

---
+++ tests/Feature/AddressTest.php
@@ -0,0 +1,273 @@
+<?php
+
+use App\Models\Address;
+use App\Models\Contact;
+use App\Models\User;
+use Database\Seeders\AddressSeeder;
+use Database\Seeders\ContactSeeder;
+use Database\Seeders\UserSeeder;
+
+use function Pest\Laravel\delete;
+use function Pest\Laravel\get;
+use function Pest\Laravel\post;
+use function Pest\Laravel\put;
+use function PHPUnit\Framework\assertEquals;
+use function PHPUnit\Framework\assertNull;
+
+describe('create address', function () {
+    it('success', function () {
+        $this->seed([UserSeeder::class, ContactSeeder::class]);
+        $user = getUserSeed();
+        $contact = getContact();
+
+        $data = [
+            'street' => 'testing',
+            'country' => 'Indonesia',
+            'postal_code' => '67184'
+        ];
+
+        post("/api/contacts/{$contact['id']}/addresses", $data, headers: [
+            'Authorization' => $user->token
+        ])->assertStatus(201)->json();
+    });
+    it('failed cause contact id not found', function () {
+        $this->seed([UserSeeder::class, ContactSeeder::class]);
+        $user = getUserSeed();
+        $contact = getContact();
+
+        $data = [
+            'street' => 'testing',
+            'country' => 'Indonesia',
+            'postal_code' => '67184'
+        ];
+        $contact['id'] = $contact['id'] + 1;
+
+        post("/api/contacts/{$contact['id']}/addresses", $data, headers: [
+            'Authorization' => $user->token
+        ])->assertNotFound();
+    });
+    it('failed cause authorization header not set', function () {
+        $this->seed([UserSeeder::class, ContactSeeder::class]);
+        $contact = getContact();
+
+        $data = [
+            'street' => 'testing',
+            'country' => 'Indonesia',
+            'postal_code' => '67184'
+        ];
+
+        post("/api/contacts/{$contact['id']}/addresses", $data, headers: [
+            'Authorization' => 'salah'
+        ])->assertUnauthorized();
+    });
+    it('failed cause body request is empty', function () {
+        $this->seed([UserSeeder::class, ContactSeeder::class]);
+        $user = getUserSeed();
+        $contact = getContact();
+
+        $data = [];
+
+        post("/api/contacts/{$contact['id']}/addresses", $data, headers: [
+            'Authorization' => $user->token
+        ])->assertBadRequest();
+    });
+});
+
+describe('get address', function () {
+    it('success', function () {
+        $this->seed([UserSeeder::class, ContactSeeder::class, AddressSeeder::class]);
+        $user = getUserSeed();
+        $contact = getContact();
+        $address = getAddress();
+
+        get("/api/contacts/{$contact->id}/addresses/{$address['id']}", headers: [
+            'Authorization' => $user->token
+        ])->assertOk();
+    });
+    it('failed cause address id not found', function () {
+        $this->seed([UserSeeder::class, ContactSeeder::class, AddressSeeder::class]);
+        $user = getUserSeed();
+        $contact = getContact();
+        $address = getAddress();
+        $addressId = $address['id'] + 1;
+
+        get("/api/contacts/{$contact->id}/addresses/{$addressId}", headers: [
+            'Authorization' => $user->token
+        ])->assertNotFound();
+    });
+    it('failed cause contact id not found', function () {
+        $this->seed([UserSeeder::class, ContactSeeder::class, AddressSeeder::class]);
+        $user = getUserSeed();
+        $contact = getContact();
+        $contact->id = $contact->id + 1;
+        $address = getAddress();
+
+        get("/api/contacts/{$contact->id}/addresses/{$address['id']}", headers: [
+            'Authorization' => $user->token
+        ])->assertNotFound();
+    });
+    it('failed cause token is invalid', function () {
+        $this->seed([UserSeeder::class, ContactSeeder::class, AddressSeeder::class]);
+        $contact = getContact();
+        $address = getAddress();
+
+        get("/api/contacts/{$contact->id}/addresses/{$address['id']}", headers: [
+            'Authorization' => 'salah'
+        ])->assertUnauthorized();
+    });
+});
+
+describe('update address', function () {
+    it('success', function () {
+        $this->seed([UserSeeder::class, ContactSeeder::class, AddressSeeder::class]);
+        $user = getUserSeed();
+        $contact = getContact();
+        $address = getAddress();
+
+        $dataUpdate = [
+            'street' => 'Jl Buyut Sateri',
+            'city' => 'Kab Pasuruan'
+        ];
+
+        $res = put("/api/contacts/{$contact->id}/addresses/{$address['id']}", $dataUpdate, [
+            'Authorization' => $user->token
+        ])->assertOk()->json();
+        $updatedAddress = getAddress();
+        assertEquals($updatedAddress, $res['data']);
+    });
+    it('failed cause body request empty', function () {
+        $this->seed([UserSeeder::class, ContactSeeder::class, AddressSeeder::class]);
+        $user = getUserSeed();
+        $contact = getContact();
+        $address = getAddress();
+
+        put("/api/contacts/{$contact->id}/addresses/{$address['id']}", headers: [
+            'Authorization' => $user->token
+        ])->assertBadRequest()->assertJson([
+            'errors' => [
+                'message' => ['no data provide!']
+            ]
+        ]);
+    });
+    it('failed cause contact id not found', function () {
+        $this->seed([UserSeeder::class, ContactSeeder::class, AddressSeeder::class]);
+        $user = getUserSeed();
+        $contact = getContact();
+        $contact->id = $contact->id + 1;
+        $address = getAddress();
+
+        $dataUpdate = [
+            'street' => 'Jl Buyut Sateri',
+            'city' => 'Kab Pasuruan'
+        ];
+
+        put("/api/contacts/{$contact->id}/addresses/{$address['id']}", $dataUpdate, headers: [
+            'Authorization' => $user->token
+        ])
+            ->assertNotFound()->assertJson([
+                'errors' => [
+                    'message' => ['not found!']
+                ]
+            ]);
+    });
+    it('failed cause address id not found', function () {
+        $this->seed([UserSeeder::class, ContactSeeder::class, AddressSeeder::class]);
+        $user = getUserSeed();
+        $contact = getContact();
+        $address = getAddress();
+        $address['id'] = $address['id'] + 1;
+
+        $dataUpdate = [
+            'street' => 'Jl Buyut Sateri',
+            'city' => 'Kab Pasuruan'
+        ];
+
+        put("/api/contacts/{$contact->id}/addresses/{$address['id']}", $dataUpdate, headers: [
+            'Authorization' => $user->token
+        ])->assertNotFound()->assertJson([
+            'errors' => [
+                'message' => ['not found!']
+            ]
+        ]);
+    });
+    it('failed cause token is invalid', function () {
+        $this->seed([UserSeeder::class, ContactSeeder::class, AddressSeeder::class]);
+        $contact = getContact();
+        $address = getAddress();
+
+        put("/api/contacts/{$contact->id}/addresses/{$address['id']}", headers: [
+            'Authorization' => 'salah'
+        ])->assertUnauthorized()->assertJson([
+            'errors' => [
+                'message' => ['unauthorized']
+            ]
+        ]);
+    });
+});
+
+describe('delete addresses', function () {
+    it('success', function () {
+        $this->seed([UserSeeder::class, ContactSeeder::class, AddressSeeder::class]);
+        $user = getUserSeed();
+        $contact = getContact();
+        $address = getAddress();
+        delete("/api/contacts/{$contact->id}/addresses/{$address['id']}", headers: [
+            'Authorization' => $user->token
+        ])->assertOk()->assertJson([
+            'data' => true
+        ]);
+
+        assertNull(getAddress());
+    });
+    it('failed cause contacts id not found', function () {
+        $this->seed([UserSeeder::class, ContactSeeder::class, AddressSeeder::class]);
+        $user = getUserSeed();
+        $contact = getContact();
+        $contactId = $contact->id + 1;
+        $address = getAddress();
+        delete("/api/contacts/{$contactId}/addresses/{$address['id']}", headers: [
+            'Authorization' => $user->token
+        ])->assertNotFound()->assertJson([
+            'errors' => ['message' => ['not found!']]
+        ]);
+    });
+    it('failed cause address id not found', function () {
+        $this->seed([UserSeeder::class, ContactSeeder::class, AddressSeeder::class]);
+        $user = getUserSeed();
+        $contact = getContact();
+        $address = getAddress();
+        $addressId = $address['id'] + 1;
+        delete("/api/contacts/{$contact->id}/addresses/{$addressId}", headers: [
+            'Authorization' => $user->token
+        ])->assertNotFound()->assertJson([
+            'errors' => ['message' => ['not found!']]
+        ]);
+    });
+    it('failed cause token is invalid', function () {
+        $this->seed([UserSeeder::class, ContactSeeder::class, AddressSeeder::class]);
+        $contact = getContact();
+        $address = getAddress();
+        delete("/api/contacts/{$contact->id}/addresses/{$address['id']}", headers: [
+            'Authorization' => 'salah'
+        ])->assertUnauthorized()->assertJson([
+            'errors' => ['message' => ['unauthorized']]
+        ]);
+    });
+})->only();
+
+
+
+function getUserSeed()
+{
+    return User::where('username', 'tado3002')->first();
+}
+
+function getContact()
+{
+    return Contact::limit(1)->first();
+}
+function getAddress()
+{
+    $address = Address::limit(1)->select(['street', 'city', 'province', 'country', 'postal_code', 'id'])->first();
+    return $address == null ? null : $address->toArray();
+}
